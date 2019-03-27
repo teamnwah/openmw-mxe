3db2c314b554ce476f5413e05edef76e5bb529e2
eater <=@eater.me>
2019-03-27 21:09:10 +0100

    target 0.44 for now

--- mxe.src/bullet.mk
+++ mxe.src/bullet.mk
@@ -5,10 +5,10 @@ $(PKG)_WEBSITE  := https://bulletphysics.org/
 $(PKG)_DESCR    := Bullet physics, version 2
 $(PKG)_VERSION  := 2.86
 $(PKG)_IGNORE   :=
-$(PKG)_SUBDIR		:= $(PKG)3-$($(PKG)_VERSION)
+$(PKG)_SUBDIR   := $(PKG)3-$($(PKG)_VERSION)
 $(PKG)_CHECKSUM := e6e8b755280ce2c1a8218529eae5dd78e184f7036854229cea611374ad5a671f
 $(PKG)_GH_CONF  := bulletphysics/bullet3/releases
-$(PKG)_URL_2		:= https://github.com/bulletphysics/bullet3/archive/$($(PKG)_VERSION).tar.gz
+$(PKG)_URL_2    := https://github.com/bulletphysics/bullet3/archive/$($(PKG)_VERSION).tar.gz
 $(PKG)_DEPS     := cc
 $(PKG)_PATCHES  :=
 
@@ -17,7 +17,7 @@ define $(PKG)_BUILD
         -DINSTALL_LIBS=ON \
         -DBUILD_CPU_DEMOS=OFF \
         -DBUILD_DEMOS=OFF \
-				-DBUILD_BULLET2_DEMOS=OFF \
+        -DBUILD_BULLET2_DEMOS=OFF \
         -DBUILD_EXTRAS=OFF \
         -DBUILD_MULTITHREADING=OFF \
         -DBUILD_UNIT_TESTS=OFF \
@@ -28,7 +28,7 @@ define $(PKG)_BUILD
     $(MAKE) -C '$(1)' -j '$(JOBS)' install VERBOSE=1
 
     '$(TARGET)-g++'  \
-				-Wall -Wextra -std=gnu++0x \
+        -Wall -Wextra -std=gnu++0x \
         '$(TEST_FILE)' -o '$(PREFIX)/$(TARGET)/bin/test-$(PKG).exe' \
         `'$(TARGET)-pkg-config' $(PKG) --cflags --libs`
 endef
--- mxe.src/openmw.mk
+++ mxe.src/openmw.mk
@@ -1,39 +1,37 @@
 PKG             := openmw
 $(PKG)_DESCR    := Fast, flexible and simple GUI.
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 0.45.0
-$(PKG)_SUBDIR		:= openmw-openmw-$($(PKG)_VERSION)
-$(PKG)_CHECKSUM := b63cf971f406ef5f28019f65e9e2bd9641a227459ede45d147562917f67e1c64
+$(PKG)_VERSION  := 0.44.0
+$(PKG)_SUBDIR   := openmw-openmw-$($(PKG)_VERSION)
+$(PKG)_CHECKSUM := 9e7db99dd3e1434b2e32e9e61ae5a8fb516a69bb6880fb2856c09f2c9ca8cf0d
 $(PKG)_GH_CONF  := openmw/openmw/releases
-$(PKG)_URL_2		:= https://github.com/openmw/openmw/archive/openmw-$($(PKG)_VERSION).tar.gz
+$(PKG)_URL_2    := https://github.com/openmw/openmw/archive/openmw-$($(PKG)_VERSION).tar.gz
 $(PKG)_DEPS     := cc bullet qt openscenegraph boost ffmpeg openal sdl2 mygui
-$(PKG)_BINARY_LOCATION := $(PREFIX)/$(TARGET)/opt/openmw
 
 define $(PKG)_BUILD
-
     cd '$(BUILD_DIR)' && $(TARGET)-cmake '$(SOURCE_DIR)' \
-			-DMyGUI_LIBRARY=$(PREFIX)/$(TARGET)/lib/Release/libMyGUIEngineStatic.a \
-			-DMYGUI_STATIC=ON \
-			-DBOOST_STATIC=ON \
-			-DSDL2_STATIC=ON \
-			-DOSG_STATIC=ON \
-			-DQT_STATIC=ON \
-			-DRECASTNAVIGATION_STATIC=ON \
-			-DBUILD_OPENCS=OFF \
-			-DBUILD_NIFTEST=OFF \
-			-DBUILD_LAUNCHER=OFF \
-			-DBUILD_WIZARD=OFF \
-			-DBUILD_MWINIIMPORTER=OFF \
-			-DBUILD_ESSIMPORTER=OFF \
-			-DBUILD_BSATOOL=OFF \
-			-DBUILD_ESMTOOL=OFF \
-			-DBoost_ALL_NO_LIB=ON \
-			-DCMAKE_BUILD_TYPE=None \
-			-DBUILD_SHARED_LIBS=OFF \
-			-DCMAKE_CXX_FLAGS="-static -Wa,-mbig-obj -Wl,-Bstatic -DAL_LIBTYPE_STATIC" \
-			-DOPENMW_CUSTOM_FLAGS="-Wl,-Bstatic $(PREFIX)/$(TARGET)/lib/libOpenAL32.a `$(TARGET)-pkg-config --cflags --libs openal freetype2 sdl2 gl libavcodec libavdevice libavfilter libavformat libavresample libavutil`"
-		$(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)' VERBOSE=1
+      -DMyGUI_LIBRARY=$(PREFIX)/$(TARGET)/lib/Release/libMyGUIEngineStatic.a \
+      -DMYGUI_STATIC=ON \
+      -DBOOST_STATIC=ON \
+      -DSDL2_STATIC=ON \
+      -DOSG_STATIC=ON \
+      -DOSG_PLUGINS_DIR=$(PREFIX)/$(TARGET)/lib/osgPlugins-${openscenegraph_VERSION}.0 \
+      -DQT_STATIC=ON \
+      -DRECASTNAVIGATION_STATIC=ON \
+      -DBUILD_OPENCS=OFF \
+      -DBUILD_NIFTEST=OFF \
+      -DBUILD_LAUNCHER=OFF \
+      -DBUILD_WIZARD=OFF \
+      -DBUILD_MWINIIMPORTER=OFF \
+      -DBUILD_ESSIMPORTER=OFF \
+      -DBUILD_BSATOOL=OFF \
+      -DBUILD_ESMTOOL=OFF \
+      -DBUILD_SHARED_LIBS=OFF \
+      -DCMAKE_CXX_FLAGS="-static -Wa,-mbig-obj -Wl,-Bstatic -DAL_LIBTYPE_STATIC" \
+      -DOPENMW_STATIC=ON \
+      -DOPENMW_CXX_FLAGS="-Wl,-Bstatic `$(TARGET)-pkg-config --static --cflags --libs libjpeg openal freetype2 sdl2 gl libavcodec libavdevice libavfilter libavformat libavresample libavutil`"
+    $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)' VERBOSE=1
     $(TARGET)-strip '$(BUILD_DIR)/openmw.exe'
-    mkdir -p '$($(PKG)_BINARY_LOCATION)'
-    cp '$(BUILD_DIR)/openmw.exe' '$($(PKG)_BINARY_LOCATION)'
+    mkdir -p '$(PREFIX)/$(TARGET)/opt/openmw'
+    cp '$(BUILD_DIR)/openmw.exe' '$(PREFIX)/$(TARGET)/opt/openmw'
 endef
